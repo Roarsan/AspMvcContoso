da158cee3e14b213073d5734afc7ada0273d9c7c
Roshan Gauchan <91730218+Roarsan@users.noreply.github.com>
2022-12-09 11:45:33 +0000

    LB3SS

--- AspMvcContoso/Models/Student.cs
+++ AspMvcContoso/Models/Student.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace AspMvcContoso.Models
 { 
     public class Student
     {
         public int ID { get; set; }
+        [Required, StringLength(20)]
         public string LastName { get; set; }
+        [Required, StringLength(20)]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
         //Relationships or navigation properties
         public virtual ICollection<Enrollment> Enrollments { get; set; }
